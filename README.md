dbfda121398a597c301efb56534e97fea7416b03
computer0101 <deveshpatel7643@gmail.com>
2018-06-02 17:46:39 +0530

    added more interactions and animations

--- README.md
+++ README.md
@@ -3,15 +3,29 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
+* [Dependencies](#dependencies)
 * [Contributing](#contributing)
 
 ## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
+1. After page load whenever you will click on card for the first time timer will start.
+2. Try to match cards by opening any two.
+3. If both card matches they will be set to open else it will flip back and you will have to remember which card was where to hit correct ones next time.
+4. Once all cards are matched you will be prompted with your score.
+5. You can see your current time, score and moves below the title of game.
+6. Star rating is as follow: 
+  	* Three stars for 0 to 10 moves.
+  	* Two stars for 11 to 14 moves.
+  	* One stars for 15 and more moves.
+7. You can also restart the game by clicking reset icon.
 
 For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+
+## Dependencies
+* [Wow JS](https://github.com/matthieua/WOW)
+* [Animate CSS](https://github.com/daneden/animate.css)
+* [Font Awesome](https://fontawesome.com/)
+* [Google Fonts](https://fonts.google.com/)
+* [jQuery](http://jquery.com/)
 
 ## Contributing
 
--- js/app.js
+++ js/app.js
@@ -1,22 +1,31 @@
-var mins = 0;
-var secs = 0;
-var array = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb']
-var domMins = $('#mins');
-var domSecs = $('#secs');
-var domMoves = $('.moves');
-var click = 0;
-var done = 0;
-var moves = 0;
-var interval;
-var firstClick = 0;
-var openedCards = [];
-var checkClasses = [];
+var mins = 0,
+    secs = 0,
+    array = ['fa fa-diamond', 'fa fa-paper-plane-o',
+        'fa fa-anchor', 'fa fa-bolt',
+        'fa fa-cube', 'fa fa-leaf',
+        'fa fa-bicycle', 'fa fa-bomb',
+        'fa fa-diamond', 'fa fa-paper-plane-o',
+        'fa fa-anchor', 'fa fa-bolt',
+        'fa fa-cube', 'fa fa-leaf',
+        'fa fa-bicycle', 'fa fa-bomb'
+    ],
+    domMins = $('#mins'),
+    domSecs = $('#secs'),
+    domMoves = $('.moves'),
+    click = 0,
+    done = 0,
+    moves = 0,
+    interval, firstClick = 0,
+    openedCards = [],
+    checkClasses = [];
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+//Initialize 'wow'.
+(new WOW).init();
+
+// Shuffle function
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -24,93 +33,15 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
-window.onload = startGame();
-
-function startGame() {
-    array = shuffle(array);
-    $('.deck li i').removeClass();
-    var temporary = $('.deck li i');
-    var j = 0;
-    for (i = 0; i < temporary.length; i++) {
-        $(temporary[i]).addClass(array[j]);
-        if (i === 7) {
-            shuffle(array);
-            j = -1;
-        }
-        j++;
-    }
-}
-
-$('.restart').on('click', function() { resetGame(false) });
-
-$('.deck').on('click', 'li', function(e) {
-    if (firstClick === 0) {
-        startTimer();
-        firstClick++;
-    }
-    if ((!alreadyOpened(this)) && (click < 2)) {
-        openedCards[click] = this;
-        click++;
-        click === 2 ? $(domMoves).text(++moves) : null; //set moves after every 2 clicks
-        $(this).addClass('show open');
-        $(domMoves).text(moves);
-        var that = this;
-        setTimeout(function() {
-            $(openedCards[click - 1]).removeAttr('style');
-            if (click == 2) {
-                checkMatchingCards();
-            } else {
-                for (i = 0; i < 2; i++) {
-                    $(openedCards[i]).removeClass('animated');
-                }
-            }
-        }, 300);
-    }
-});
-
-function checkMatchingCards() {
-    for (i = 0; i < 2; i++) {
-        checkClasses[i] = $(openedCards[i]).children();
-        checkClasses[i] = $(checkClasses[i]).attr('class');
-    }
-    if (checkClasses[0] === checkClasses[1]) {
-        setCards();
-    } else {
-        hideCards();
-    }
-}
-
-function hideCards() {
-    setTimeout(function() {
-        $(openedCards[0]).removeClass('show open');
-        $(openedCards[1]).removeClass('show open');
-        click = 0;
-    }, 400);
-}
-
-function setCards() {
-    for (i = 0; i < 2; i++) {
-        $(openedCards[i]).removeClass('show open');
-        $(openedCards[i]).addClass('match');
-    }
-    setTimeout(function() {
-        click = 0;
-        done++;
-        if (done === 8) {
-            resetGame(true);
-        }
-    }, 30);
-}
-
+//checks if the clicked card is already opened. It is called in event listener of click on card. line 60
 function alreadyOpened(card) {
     var temp = $(card).attr('class');
     temp = temp.split(' ');
     for (i = 0; i < temp.length; i++) {
-        if (temp[i] === 'match') {
+        if (temp[i] === 'match' || temp[i] === 'open') {
             return true;
         } else if (i === temp.length - 1) {
             return false;
@@ -118,6 +49,7 @@ function alreadyOpened(card) {
     }
 }
 
+//handles and keeps track of time when the game starts and stops when it is over.
 function startTimer() {
     interval = setInterval(function() {
         secs++;
@@ -130,17 +62,113 @@ function startTimer() {
     }, 1000);
 }
 
+//checks and sets stars on page based on current number of moves.
+function checkStars() {
+    if (moves === 11) {
+        $('.stars').children()[2].remove();
+    } else if (moves === 15) {
+        $('.stars').children()[1].remove();
+    }
+}
+
+//Resets the game.
 function resetGame(type) {
     clearInterval(interval);
     secs = secs < 10 ? ('0' + secs) : secs;
     mins = mins < 10 ? ('0' + mins) : mins;
+    var stars = moves < 11 ? (3) : (moves < 15 ? (2) : (1));
     if (type) {
-        alert('Time taken: ' + mins + ':' + secs + '.' + 'You took in total ' + moves + '.');
+        alert('Congratulations! You won.\nTime taken: ' + mins + ' : ' + secs + '.\nYou took in total ' + moves + ' moves.\n Total Stars: ' + stars + '.');
     }
-    mins = secs = click = firstClick = moves = 0;
+    mins = secs = click = firstClick = moves = done = 0;
     domSecs.text('00');
     domMins.text('00');
     domMoves.text('0');
+    $('.stars').html('<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>');
     $('.deck li').removeClass('match open show');
     startGame();
 }
+
+//handles the functionality of hiding the cards if they did not match.
+function hideCards() {
+    setTimeout(function() {
+        $(openedCards[0]).removeClass('show open shake');
+        $(openedCards[1]).removeClass('show open shake');
+        click = 0;
+    }, 400);
+}
+
+//handles the functionality of setting the cards if they are matching.
+function setCards() {
+    for (i = 0; i < 2; i++) {
+        $(openedCards[i]).removeClass('show open rubberBand');
+        $(openedCards[i]).addClass('match');
+    }
+    setTimeout(function() {
+        click = 0;
+        done++;
+        if (done === 8) {
+            resetGame(true); //checks if all cards are opened by checking 'done'
+        }
+    }, 30);
+}
+
+//handles funcitonality of checking opened cards.
+function checkMatchingCards() {
+    for (i = 0; i < 2; i++) {
+        checkClasses[i] = $(openedCards[i]).children();
+        checkClasses[i] = $(checkClasses[i]).attr('class');
+    }
+    if (checkClasses[0] === checkClasses[1]) { //if two opened cards are same then set them to open...
+        $(openedCards[0]).addClass('rubberBand');
+        $(openedCards[1]).addClass('rubberBand');
+        setTimeout(() => {
+            setCards(); //calling to set cards open as they are matched.
+        }, 300);
+    } else { //if two opened cards are different then hide them...
+        $(openedCards[0]).addClass('shake');
+        $(openedCards[1]).addClass('shake');
+        setTimeout(() => {
+            hideCards(); //calling to hide cards as they are not matching.
+        }, 300);
+    }
+}
+
+//Listening for click 'event' on 'reset' button
+$('.restart').on('click', function() { resetGame(false) });
+
+//Listening for click 'click' event on each card
+$('.deck').on('click', 'li', function(e) {
+    if (firstClick === 0) {
+        //Start timer when user clicks first card.
+        startTimer();
+        firstClick++;
+    }
+    if ((!alreadyOpened(this)) && (click < 2)) {
+        openedCards[click] = this;
+        click++;
+        click === 2 ? $(domMoves).text(++moves) : null; //increment and then set 'moves' after every 2 clicks...
+        click === 2 ? checkStars() : null; //check number of stars on every increment to 'moves'
+        $(this).addClass('show open flipInY');
+        $(domMoves).text(moves);
+        var that = this;
+        setTimeout(function() {
+            $(that).removeClass('flipInY');
+            if (click == 2) { //check's if two opened cards are matching.
+                checkMatchingCards();
+            }
+        }, 300);
+    }
+});
+
+//Call below function to start the game.
+function startGame() {
+    array = shuffle(array);
+    $('.deck li i').removeClass();
+    var temporary = $('.deck li i');
+    for (i = 0; i < temporary.length; i++) {
+        $(temporary[i]).addClass(array[i]);
+    }
+}
+
+window.onload = startGame;
